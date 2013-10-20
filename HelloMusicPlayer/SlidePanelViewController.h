a63361b954b177ac47694ed40cb708ecef3138bc
nm777taka <nm7.taka@gmail.com>
2013-10-20 17:14:44 +0900

    JASlidePanelControllerを追加

---
+++ HelloMusicPlayer/SlidePanelViewController.h
@@ -0,0 +1,13 @@
+//
+//  SlidePanelViewController.h
+//  HelloMusicPlayer
+//
+//  Created by 古田 貴久 on 2013/10/20.
+//  Copyright (c) 2013年 古田 貴久. All rights reserved.
+//
+
+#import "JASidePanelController.h"
+
+@interface SlidePanelViewController : JASidePanelController
+
+@end
---
+++ HelloMusicPlayer/SlidePanelViewController.m
@@ -0,0 +1,38 @@
+//
+//  SlidePanelViewController.m
+//  HelloMusicPlayer
+//
+//  Created by 古田 貴久 on 2013/10/20.
+//  Copyright (c) 2013年 古田 貴久. All rights reserved.
+//
+
+#import "SlidePanelViewController.h"
+
+@interface SlidePanelViewController ()
+
+@end
+
+@implementation SlidePanelViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
